0d9487a4bf06ad975214d11d4b06af15c09b4e7c
Dragos Protung <dragos@protung.ro>
2017-11-17 15:37:54 +0100

    Overwrite easyadmin_autocomplete form type to allow custom choice label and value

--- src/DependencyInjection/CompilerPass/EasyAdminOverwrite.php
+++ src/DependencyInjection/CompilerPass/EasyAdminOverwrite.php
@@ -6,6 +6,7 @@ namespace Wingu\EasyAdminPlusBundle\DependencyInjection\CompilerPass;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Wingu\EasyAdminPlusBundle\Form\Type\EasyAdminAutocompleteType;
 use Wingu\EasyAdminPlusBundle\Twig\Extension\EasyAdminTwigExtension;
 
 final class EasyAdminOverwrite implements CompilerPassInterface
@@ -18,5 +19,9 @@ final class EasyAdminOverwrite implements CompilerPassInterface
         $container
             ->getDefinition('easyadmin.twig.extension')
             ->setClass(EasyAdminTwigExtension::class);
+
+        $container
+            ->getDefinition('easyadmin.form.type.autocomplete')
+            ->setClass(EasyAdminAutocompleteType::class);
     }
 }
---
+++ src/Form/Type/EasyAdminAutocompleteType.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Wingu\EasyAdminPlusBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EasyAdminAutocompleteType extends \EasyCorp\Bundle\EasyAdminBundle\Form\Type\EasyAdminAutocompleteType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefined(
+            \array_merge(
+                $resolver->getDefinedOptions(),
+                ['choice_label', 'choice_value']
+            )
+        );
+    }
+}
