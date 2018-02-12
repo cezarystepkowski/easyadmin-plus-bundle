eefb4cd288814ee06cadd711f88c864823e3d956
Dragos Protung <dragos@protung.ro>
2018-02-12 11:06:19 +0100

    Do not autowire security controller is FOS user bundle is not installed

--- src/DependencyInjection/WinguEasyAdminPlusExtension.php
+++ src/DependencyInjection/WinguEasyAdminPlusExtension.php
@@ -1,13 +1,15 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Wingu\EasyAdminPlusBundle\DependencyInjection;
 
+use FOS\UserBundle\Controller\SecurityController as FOSSecurityController;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
+use Wingu\EasyAdminPlusBundle\Controller\SecurityController as WinguSecurityController;
 
 class WinguEasyAdminPlusExtension extends ConfigurableExtension
 {
@@ -21,5 +23,9 @@ class WinguEasyAdminPlusExtension extends ConfigurableExtension
         $container->setParameter('wingu_easy_admin_plus.logo', $config['logo']);
         $container->setParameter('wingu_easy_admin_plus.title', $config['title']);
         $container->setParameter('wingu_easy_admin_plus.advanced_search_form_class', $config['advanced_search_form_class']);
+
+        if (!\class_exists(FOSSecurityController::class)) {
+            $container->removeDefinition(WinguSecurityController::class);
+        }
     }
 }
